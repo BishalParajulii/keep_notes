a9c2bb25c282db9478a90e67411d32f800533d8c
BishalParajulii <parajulibishal00@gmail.com>
2025-09-26 20:46:45 +0545

    working on frontend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path , include
-from .views import CreateUserView
+from .views import CreateUserView , NoteListCreate , NoteDelete
 
 urlpatterns = [
-    path("api/user/register/" , CreateUserView.as_view() , name="register")
+    path("api/user/register/" , CreateUserView.as_view() , name="register"),
+    path("notes/" , NoteListCreate.as_view() , name="note-list"),
+    path("note/delete/<int:pk>/" , NoteDelete.as_view() , name="delete-note")
 ]
